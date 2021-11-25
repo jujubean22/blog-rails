da0c5da0377b76befca8ae776a522adf26e5c530
jujubean22 <jmevillanueva22@gmail.com>
2021-11-25 20:20:59 +0800

    added comments in migration and more validtion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_032925) do
+ActiveRecord::Schema.define(version: 2021_11_25_115529) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2021_11_13_032925) do
     t.datetime "created_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.boolean "toxic", default: false
+    t.integer "article_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
