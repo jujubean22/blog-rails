5e97c227155c002bb6801e79b8595fb385a15f4c
jujubean22 <jmevillanueva22@gmail.com>
2022-01-03 19:58:33 +0800

    updated database for deployment

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,14 @@ class Article < ApplicationRecord
     validates :title, presence: true, uniqueness: {scope: 'title', message:'should be unique'}
 
     has_many :comments
+    has_and_belongs_to_many :tags
+
+    before_validation :set_content
+
+    def set_content
+        unless self.content
+        self.content = 'Default Content'
+        end
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_115529) do
+ActiveRecord::Schema.define(version: 2021_12_06_123930) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2021_11_25_115529) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "articles_tags", id: false, force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.integer "tag_id", null: false
+    t.index ["article_id", "tag_id"], name: "index_articles_tags_on_article_id_and_tag_id"
+    t.index ["tag_id", "article_id"], name: "index_articles_tags_on_tag_id_and_article_id"
+  end
+
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.boolean "toxic", default: false
@@ -29,6 +36,12 @@ ActiveRecord::Schema.define(version: 2021_11_25_115529) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
